1e3e8c5a5f0bcc9f7004e4638fb3f7605f884793
Nele Custers <custersnele@gmail.com>
2024-10-14 08:26:14 +0200

    removed unused imports

--- src/main/java/be/pxl/pets/api/PetController.java
+++ src/main/java/be/pxl/pets/api/PetController.java
@@ -1,21 +1,12 @@
 package be.pxl.pets.api;
 
-import be.pxl.pets.api.request.PetCreateRequest;
 import be.pxl.pets.domain.Pet;
-import be.pxl.pets.exceptions.PetTooHungryException;
-import be.pxl.pets.exceptions.PetTooTiredException;
 import be.pxl.pets.service.PetService;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
